0ceeab98ebe4f1f4c837a14bd6adb8a7b8c39a42
Aditya Dhayagude <adityadhayagude03@gmail.com>
2024-07-03 12:26:48 +0530

    Request and body Validation was added

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,12 +1,22 @@
 const express = require ('express')
 const router = express.Router()
+const { check, validationResult } = require("express-validator");
 
-// Route GET api/users
-//desc TEST Route
+// Route POSTapi/users
+//desc Register Route
 // @acess Public 
 
-router.get('/',(req,res)=>{
+router.post('/',[
+    check('name','Name is required').not().isEmpty(),
+    check('email','Please include Email').isEmail(),
+    check('password','Please Enter passoword of min Lenght of 6').isLength({min:6})
+
+],(req,res)=>{
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(400).json({errors:errors.array()})
+    }
     res.send('User Route')
-})
+});
 
 module.exports = router;
