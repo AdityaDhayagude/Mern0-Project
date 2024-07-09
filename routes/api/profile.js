af8d93d7d01c708ee6a8377732222e5d0097927f
Aditya Dhayagude <adityadhayagude03@gmail.com>
2024-07-09 09:49:26 +0530

    Put Request for Adding in experience added

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -156,4 +156,44 @@ router.delete('/',auth,async(req,res)=>{
   }
 })
 
+// Route PUT api/profile/experience
+//desc PUT profile experience
+// @acess Private
+
+router.put('/experience',
+  auth,
+  check('title', 'Title is required').notEmpty(),
+  check('company', 'Company is required').notEmpty(),
+  check('from', 'From date is required and needs to be from the past')
+    .notEmpty()
+    .custom((value, { req }) => (req.body.to ? value < req.body.to : true)),async(req,res)=>{
+
+  const errors = validationResult(req)
+  if(!errors.isEmpty()){
+    return res.status(400).json({msg:errors.array()})
+  }
+  
+  const {
+    title, company, location, from, to, current, description
+    } = req.body
+
+  const newExp = {
+      title, company, location, from, to, current, description
+    }
+
+  try {
+    const profile = await Profile.findOne({user:req.user.id})
+
+    profile.experience.unshift(newExp)
+
+    await profile.save()
+    res.json(profile)
+
+  } catch (err) {
+    console.log(err.message);
+    res.status(500).send('Server Error')
+  }
+
+})
+
 module.exports = router;
