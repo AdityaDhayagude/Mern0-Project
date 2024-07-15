39709867a44fbb11bd51e9b09844c4181f39b6c9
Aditya Dhayagude <adityadhayagude03@gmail.com>
2024-07-15 15:51:36 +0530

    Router Setup donw

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,21 @@
 import { Fragment } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './components/layouts/Navbar';
 import Landing from './components/layouts/Landing';
+import Login from './components/auth/Login';
+import Register from './components/auth/Register';
 import './App.css';
 
 const App =()=> (
   <Fragment>
-    <Navbar/>
-    <Landing/>
+    <Router>
+      <Navbar/>
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="register" element={<Register />} />
+        <Route path="login" element={<Login />} />
+      </Routes>
+    </Router>
   </Fragment>
 )
 
